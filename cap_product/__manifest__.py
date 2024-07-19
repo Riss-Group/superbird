11b3f5e7cd148282101b5da8474d4bf858ce6420
captivea-asef <168564747+captivea-asef@users.noreply.github.com>
2024-07-19 10:41:45 -0400

    [add] make product_category name and complete name translatable

---
+++ cap_product/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "CAP Product",
+    'summary': "CAP Product",
+    'description': """
+    TBD
+    """,
+    'author': 'Captivea software Consulting',
+    'website': 'https://www.captivea.com/',
+    'category': 'Uncategorized',
+    'version': '1.0',
+    'depends': ['product'],
+
+    # always loaded
+    'data': [
+    ],
+
+}
+
