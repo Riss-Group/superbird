df70e449a847176aba9b5bb5d53872938a61fed7
captivea-asef <168564747+captivea-asef@users.noreply.github.com>
2024-07-23 15:00:56 -0400

    Update product_category.py

--- cap_product/models/product_category.py
+++ cap_product/models/product_category.py
@@ -7,6 +7,3 @@ class ProductCategory(models.Model):
     _inherit = 'product.category'
 
     name = fields.Char('Name', index='trigram', required=True, translate=True)
-    complete_name = fields.Char(
-        'Complete Name', compute='_compute_complete_name', recursive=True,
-        store=True, translate=True)
