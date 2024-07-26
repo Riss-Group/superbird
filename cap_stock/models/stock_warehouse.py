973a4e0f6689fec919101042141dc4ad29d9e217
Bassim Elsamaloty <99155821+Captivea-BEL@users.noreply.github.com>
2024-07-26 14:52:45 -0400

    [ADD] - 143594 - Reordering Rule Python Code (#22)
    [ADD] - 143594 - Reordering Rule Python Code
    
    Task:
    
    https://www.captivea.com/web#id=143594&cids=1%2C14&menu_id=462&action=665&active_id=1926&model=project.task&view_type=form
    
    Deployment Notes:
    
    install / upgrade cap_stock
    
    Release Notes:
    
    Feedback to address from PM
    - Seperate out python code into its own model that is selectable on warehouse/reordering rules.
    - Remove simulation fields

--- cap_stock/models/stock_warehouse.py
+++ cap_stock/models/stock_warehouse.py
@@ -1,47 +1,13 @@
 from odoo import models, api, fields
-from odoo.exceptions import ValidationError, UserError 
-from odoo.tools.safe_eval import safe_eval, test_python_expr, wrap_module
-from odoo.addons.base.models.ir_actions import LoggerProxy
-import math
-import logging 
-_logger = logging.getLogger()
+
 
 class StockWarehouse(models.Model):
     _inherit = 'stock.warehouse'
 
 
-    reorder_order_cost_var = fields.Float()
-    reorder_inventory_maint_cost_var = fields.Float()
-    reorder_period_days = fields.Integer()
-    demo_sales_volume = fields.Float()
-    demo_unit_cost = fields.Float()
-    demo_reorder_min = fields.Float()
-    demo_reorder_max = fields.Float()
     formula_type = fields.Selection([('fixed','Fixed'), ('python_code', 'Python Code')], default="fixed")
-    python_code = fields.Text(string='Python Code', )
+    python_code_id = fields.Many2one('reordering.rule.python.code', string='Python Code')
 
-
-    @api.constrains('python_code')
-    def _check_python_code(self):
-        '''
-            Validate the syntax of the Python code in the 'python_code' field.
-            Raises a ValidationError if the syntax is incorrect (AKA Unsafe Execution from Base safe_eval).S
-        '''
-        for record in self.sudo().filtered('python_code'):
-            msg = test_python_expr(expr=record.python_code.strip(), mode="exec")
-            if msg:
-                raise ValidationError(msg)
-    
-    def run_min_max(self):
-        '''
-            Simulates the demo min/max vals for 'python_code' field and set the minimum and maximum quantities for the orderpoint based on the evaluation results.
-        '''
-        for record in self:
-            if record.formula_type == 'python_code' and record.python_code:
-                eval_context = record._get_eval_context()
-                res = safe_eval(record.python_code, eval_context, mode='exec', nocopy=True)
-                record.demo_reorder_min = eval_context.get('min_qty',0)
-                record.demo_reorder_max = eval_context.get('max_qty',0)
 
     def update_params_on_reordering_rules(self):
         '''
@@ -56,35 +22,3 @@ class StockWarehouse(models.Model):
             if not vals:
                 return False
             stock_warehouse_orderpoint_ids.write(vals)
-
-    @api.model
-    def _get_eval_context(self):
-        """
-            Prepare the context used when evaluating Python code for reordering rules.
-            This includes environment variables and utility functions.
-        """
-        def log(message, level="info"):
-            with self.pool.cursor() as cr:
-                cr.execute("""
-                    INSERT INTO ir_logging(create_date, create_uid, type, dbname, name, level, message, path, line, func)
-                    VALUES (NOW() at time zone 'UTC', %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                """, (self.env.uid, 'server', self._cr.dbname, __name__, level, message, "python code", 'run_min_max_line(whs)', 'run_min_max_func(whs)'))
-
-        eval_context = self.env['ir.actions.actions']._get_eval_context()
-        model = self.env['stock.warehouse']
-        record = model.browse(self._context['active_id'])
-        eval_context.update({
-            'env': self.env,
-            'model': model,
-            'UserError': UserError,
-            'record': record,
-            'log': log,
-            '_logger': LoggerProxy,
-            'Math': wrap_module(math, dir(math)),
-            'sales_volume': record.demo_sales_volume if record else 0,
-            'unit_cost': record.demo_unit_cost if record else 0,
-            'period_days': record.reorder_period_days if record else 0,
-            'inventory_maint_cost': record.reorder_inventory_maint_cost_var if record else 0,
-            'order_cost_fixed' : record.reorder_order_cost_var if record else 0,
-        })
-        return eval_context
