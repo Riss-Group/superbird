97da8080f57378082fa00c0017c7394372d33c7d
Captivea-BEL <99155821+Captivea-BEL@users.noreply.github.com>
2024-07-26 12:31:16 -0400

    [ADD] - 143594 - Reordering Rule Python Code
    Task:
    
    https://www.captivea.com/web#id=143594&cids=1%2C14&menu_id=462&action=665&active_id=1926&model=project.task&view_type=form
    
    Deployment Notes:
    
    install cap_stock
    
    Release Notes:
    
    - Added several new fields to warehouse/reordering rules table: PO Ordering Cost, Inventory Maintenance Cost, Reordering Period in Days, and Sales Volume
    - Added the ability to have the min/max quantities on reordering rules be set dynamically based on python code.
    - These fields can be used as properties inside of the python code field
    - A cron job is set to run daily to recompute any reordering rules min/max if the formula type is set to python_code
    - When a reordering rule is created or its location updated it will automatically set to the configuration from the warehouse itself.
    - On the warehous the python code can be simulated to show what the min/max would be for a given scenario.
    - A new group was added to allow users to see the majority of these new fields and the associated buttons and configuration. The base Admin/settings group also gains visibility for it
    - The exclusions for above is the sales order volume compute field as well as the recompute min/max button are visible
    - Since the python code on the reordering rules is hard to update there is a button added to the reordering rules that will pop it out into a form view to make easier to edit

---
+++ cap_stock/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'CAP Stock',
+    'category': 'Inventory',
+    'summary': "CAP Stock",
+    'version': '1.0',
+    'author': 'Captivea software Consulting, Bassim Elsamaloty',
+    'website': 'https://www.captivea.com/',
+    'license': 'OPL-1',
+    'description': """
+        - Adds the ability to use custom formulas within reordering rules
+        """,
+    'depends': [
+        'stock',
+        ],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'data/data.xml',
+        'views/stock_warehouse.xml',
+        'views/stock_warehouse_orderpoint.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
