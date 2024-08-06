a58116c59fe0f847748390c4378a336ab4099588
captivea-kha <khalid.hazam@captivea.com>
2024-08-05 22:08:18 -0400

    [IMP] 144604 - stock_location_full_name keep root locations untouched

--- stock_location_full_name/models/stock_location.py
+++ stock_location_full_name/models/stock_location.py
@@ -6,14 +6,14 @@ class StockLocation(models.Model):
     @api.depends('name', 'location_id.complete_name', 'warehouse_id.location_name_separator')
     def _compute_complete_name(self):
         for location in self:
-            if location.location_id and location.warehouse_id and location.location_id in location.warehouse_id.lot_stock_id.child_internal_location_ids:
+            if not (location.warehouse_id and location.location_id):
+                super(StockLocation, location)._compute_complete_name()
+            if location.location_id.parent_path.startswith(location.warehouse_id.lot_stock_id.parent_path) and location.location_id != location.warehouse_id.lot_stock_id:
                 location.complete_name = ('%s%s%s' %
                                       (location.location_id.complete_name,
                                        location.warehouse_id.location_name_separator if location.warehouse_id.location_name_separator else '',
                                        location.name))
-            elif location.warehouse_id and location == location.warehouse_id.lot_stock_id:
+            else:
                 location.complete_name = ('%s/%s' %
                                       (location.location_id.complete_name,
                                        location.name))
-            else:
-                super(StockLocation, location)._compute_complete_name()
