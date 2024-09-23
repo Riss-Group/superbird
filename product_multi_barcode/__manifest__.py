af4194577fef61c918fe1ce1597d4bb5d262c840
captivea-asef <168564747+captivea-asef@users.noreply.github.com>
2024-09-23 17:06:04 -0400

    [upd] product multi barcode

---
+++ product_multi_barcode/__manifest__.py
@@ -0,0 +1,32 @@
+# © 2012-2014 Guewen Baconnier (Camptocamp SA)
+# © 2015 Roberto Lizana (Trey)
+# © 2016 Pedro M. Baeza
+# © 2018 Xavier Jimenez (QubiQ)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Multiple barcodes on products",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Camptocamp, "
+    "Trey, "
+    "Tecnativa, "
+    "QubiQ, "
+    "ADHOC SA, "
+    "Odoo Community Association (OCA)",
+    "category": "Product Management",
+    "depends": ["product","stock_barcode"],
+    "website": "https://github.com/OCA/stock-logistics-barcode",
+    "data": [
+        "views/product_view.xml",
+        "views/product_template_view.xml",
+        "security/ir.model.access.csv",
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'product_multi_barcode/static/src/*.js',
+        ],
+    },
+    "installable": True,
+    "post_init_hook": "post_init_hook",
+}
