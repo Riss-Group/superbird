d146ba9ede245655bd44c8fc4a5282fe1f310d6f
captivea-kevin <kevin.lai@captivea.us>
2024-09-24 10:15:21 -0400

    [ADD] - 146737 - Force down payment product taxes

---
+++ cap_sale/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'CAP Sale',
+    'category': 'Sales',
+    'summary': "CAP Sale",
+    'version': '1.0',
+    'author': 'Captivea software Consulting, Kevin Lai',
+    'website': 'https://www.captivea.com/',
+    'license': 'OPL-1',
+    'description': """
+This module contains customizations for common sales features.
+        """,
+    'depends': [
+        'sale',
+        ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
