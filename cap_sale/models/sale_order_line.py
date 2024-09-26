ad4e1e259bb5e8919dfa5f5a88022594de17eba7
captivea-asef <168564747+captivea-asef@users.noreply.github.com>
2024-09-26 16:11:22 -0400

    [upd] add history of replacement is SOL

--- cap_sale/models/sale_order_line.py
+++ cap_sale/models/sale_order_line.py
@@ -13,14 +13,36 @@ class SaleOrderLine(models.Model):
             res['taxes'] = self.company_id.sale_down_payment_product_id.taxes_id
         return res
             
+    def get_replacement_history(self):
+        replacement_history = []
+        current_product = self.product_id
+
+        while current_product and current_product.replacement_id:
+            replacement_history.append((current_product, current_product.replacement_id))
+            current_product = current_product.replacement_id
+
+        return replacement_history
+
     @api.onchange('product_id')
     def _onchange_product_id(self):
         if self.product_id and self.product_id.replacement_id:
-            replacement_product = self.product_id.replacement_id
-            self.product_id = replacement_product
+            replacement_history = self.get_replacement_history()
+
+            final_replacement = replacement_history[-1][1] if replacement_history else self.product_id
+
+            self.product_id = final_replacement
+
+            history_message = "Product Replacement History:\n\n"
+            history_message += "{:<30} {:>30}\n".format("Product", "Replaced By")
+            history_message += "-" * 60 + "\n"
+
+            for product, replacement in replacement_history:
+                history_message += "{:<30} {:<30}\n".format(product.name, replacement.name)
+
             return {
                 'warning': {
                     'title': "Product Replacement",
-                    'message': f"The product you selected has been replaced with {replacement_product.name}.",
+                    'message': history_message,
                 }
             }
+
