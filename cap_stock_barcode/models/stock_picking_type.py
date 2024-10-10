2d7801904484430ca1217b7bcc6dbd7f8cc4cba3
captivea-asef <168564747+captivea-asef@users.noreply.github.com>
2024-10-10 13:05:00 -0400

    [upd] 149997 quarantine location

---
+++ cap_stock_barcode/models/stock_picking_type.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, Command
+
+
+class StockPickingType(models.Model):
+    _inherit = 'stock.picking.type'
+
+    quarantine_location_id = fields.Many2one('stock.location', compute="_compute_scrap_location_id", store=False, readonly=False)
+
+    def _compute_scrap_location_id(self):
+        groups = self.env['stock.location']._read_group(
+            [('company_id', 'in', self.company_id.ids), ('scrap_location', '=', True)], ['company_id'], ['id:min'])
+        locations_per_company = {
+            company.id: stock_warehouse_id
+            for company, stock_warehouse_id in groups
+        }
+        self.quarantine_location_id =  locations_per_company[self.company_id.id]
