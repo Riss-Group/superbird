9696f99a7e04c8a439b080112d876affbb2633de
Bassim Elsamaloty <99155821+Captivea-BEL@users.noreply.github.com>
2024-10-31 10:43:11 -0400

    [ADD] - 139350 - Service Improvements (#93)
    Tasks
    - [139350 - Vehicle Warranty Lines on Fleet](https://www.captivea.com/web#id=139350&menu_id=462&cids=1%2C14&action=665&active_id=1926&model=project.task&view_type=form)
    - [151472 - Fleet Vehicle Sequencing](https://www.captivea.com/web#id=151472&menu_id=462&cids=1%2C14&action=665&active_id=1926&model=project.task&view_type=form)
    - [139328 - Link Fleet to Repair Orders](https://www.captivea.com/web#id=139328&menu_id=462&cids=1%2C14&action=665&active_id=1926&model=project.task&view_type=form)
    - [139372 - Canned Service Line Templates](https://www.captivea.com/web#id=139372&menu_id=462&cids=1%2C14&action=665&active_id=1926&model=project.task&view_type=form)
    - [139449 - Fleet Misc Fields View Edits](https://www.captivea.com/web#id=139449&menu_id=462&cids=1%2C14&action=665&active_id=1926&model=project.task&view_type=form)
    
    Deployment Notes:
    Upgrade cap_service
    
    Release Notes:
    Fleet:
    -	Updated the `Name Search` override to include body/stock/customer reference numbers
    -	Updated the `display name` for fleet records to show as Make/Model/Body Number. NA Will be displayed instead if a body number is not yet set.
    -	Added a link to the fleet record for any service orders that it is used in
    -	Removed all other smart buttons not going to be used for bus fleet if the Make/Model is a bus fleet. (EG Odometer and Documents stayed)
    -	Added all of the Serial Number Fields to the fleet that show up on the BB Vantage site and moved all Reference Number fields to its own group on the form view
    -	Added the Fleets associated product to display the selected attribute values
    Products/Attribute
    -	Added fields on the Attribute for `is_cap` and `is_bu` to be used to indicate that this attribute is used in serial number generation
    -	If `is_bu` is checked `company_code` will display on the attribute line and is required. This is used in the mapping of the serial number (EG AGI = 1, GOI = 2)
    -	Added several sequence computation fields to help with autogeneration of the stock number. The desired format should be something like (1) 24-720001. 1 = Company Code, 24 = Year, 72 = Capacity, 0001 is sequentially incremented.
    -	System admins will be able to see sequence details on the variant form view
    Purchase Order/Stock Moves
    -	The initial fleet record creation will now fire on PO confirmation
    -	Furthermore if the product is a fleeted product then the serial number will also be autogenerated at this time based on the description above for the serial number format
    -	Technically this occurs on the action_assign for a receipt
    -	Validation of the transfer will also trigger an update if information such as stock number / dates were changed since the initial creation
    -	The base serial number generator wizard was also customized to be prefilled with the sequence for fleeted records.
    Service Order
    -	Templates
    -	Create date was added to the form view
    -	Fleet Record is now required when trying to move the repair into any other state than draft
    -	Added better organization to the service templates form/list views by adding ‘Op Code’ as the name and displaying it as a title. Job Type was also added as a selection field for options of customer/warranty/internal
    -	Complaint field was readded back to the view
    -	Added the Op Code Reference to the Repair Lines

--- cap_service/models/product_template.py
+++ cap_service/models/product_template.py
@@ -10,11 +10,13 @@ class ProductTemplate(models.Model):
         current_year = fields.Date.today().year
         return [(str(year), str(year)) for year in range(current_year + 2, current_year - 26, -1)]
 
+
     create_fleet_vehicle = fields.Boolean('Create Fleet Vehicle', help="When checked, this product will automatically create a fleet vehicle record when receipted into inventory", )
     vehicle_year = fields.Selection(selection=_get_years, string="Year")
     vehicle_make_id = fields.Many2one('fleet.vehicle.model.brand', string="Make")
     vehicle_model_id = fields.Many2one('fleet.vehicle.model', string="Model")
     available_vehicle_model_ids = fields.Many2many('fleet.vehicle.model', compute='_compute_available_vehicle_model_ids')
+
 
     @api.onchange('tracking', 'purchase_ok')
     def _onchange_tracking_purchase(self):
@@ -43,3 +45,18 @@ class ProductTemplate(models.Model):
                 record.available_vehicle_model_ids = self.env['fleet.vehicle.model'].search([('is_bus_fleet','=',True)])
             else:
                 record.available_vehicle_model_ids = self.env['fleet.vehicle.model'].search([('id','in',record.vehicle_make_id.model_ids.ids)])
+    
+    def write(self, vals):
+        res = super().write(vals)
+        if vals.get('vehicle_year'):
+            for record in self:
+                record.product_variant_ids._compute_sequence_code()
+        return res
+    
+    @api.model_create_multi
+    def create(self, vals):
+        records = super().create(vals)
+        if vals.get('vehicle_year'):
+            for record in records:
+                record.product_variant_ids._compute_sequence_code()
+        return records
