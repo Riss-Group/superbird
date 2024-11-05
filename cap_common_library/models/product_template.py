bcbd8eee989b285e90e75914a5de4a527bc7b47e
Bassim Elsamaloty <99155821+Captivea-BEL@users.noreply.github.com>
2024-11-05 17:04:24 -0500

    [ADD] - 144173 - Product Fields (#104)
    Task:
    
    https://www.captivea.com/web#id=144173&cids=1%2C14&menu_id=462&action=665&active_id=1926&model=project.task&view_type=form
    
    Deployment Notes:
    
    upgrade cap_common_library
    
    Release Notes:
    
    - Consolidated view overrides for product product vs template
    - Added menus/actions for each new model created. These menu items are under 'Additional Product Configurations' under Inventory/Configuration and inherit its permissions
    - Created m2o/o2m for group/price classes.
    - The product form view will now filter sub group/price class based on the parent group/price class
    - All char/text fields were made translateable

--- cap_common_library/models/product_template.py
+++ cap_common_library/models/product_template.py
@@ -11,32 +11,49 @@ class ProductTemplate(models.Model):
     product_group_id = fields.Many2one('product.group', string='Product Group')
     product_subgroup_id = fields.Many2one('product.subgroup', string='Product Subgroup')
 
+    @api.onchange('price_class_id')
+    def _onchange_price_class_id(self):
+        if not self.price_class_id or self.price_subclass_id not in self.price_class_id.ids:
+            self.price_subclass_id = False
+
+    @api.onchange('product_group_id')
+    def _onchange_product_group_id(self):
+        if not self.product_group_id or self.product_subgroup_id not in self.product_group_id.ids:
+            self.product_subgroup_id = False
+
 class ProductStockClass(models.Model):
     _name = "product.stock_class"
     _description = "Stock Class"
 
-    name = fields.Char('Name')
+    name = fields.Char('Name', translate=True)
 
 class ProductPriceClass(models.Model):
     _name = "product.price_class"
     _description = "Price Class"
 
-    name = fields.Char('Name')
+    name = fields.Char('Name', translate=True)
+    price_subclass_ids = fields.One2many('product.price_subclass', 'price_class_id', string='Price Subclasses')
+
 
 class ProductPriceSubclass(models.Model):
     _name = "product.price_subclass"
     _description = "Price Subclass"
 
-    name = fields.Char('Name')
+    name = fields.Char('Name', translate=True)
+    price_class_id = fields.Many2one('product.price_class', string='Price Class')
+
 
 class ProductGroup(models.Model):
     _name = "product.group"
     _description = "Product Group"
 
-    name = fields.Char('Name')
-    
+    name = fields.Char('Name', translate=True)
+    product_subgroup_ids = fields.One2many('product.subgroup', 'product_group_id', string='Product Subgroups')
+
+
 class ProductSubgroup(models.Model):
     _name = "product.subgroup"
     _description = "Product Subgroup"
 
-    name = fields.Char('Name')
+    name = fields.Char('Name', translate=True)
+    product_group_id = fields.Many2one('product.group', string='Product Group')
