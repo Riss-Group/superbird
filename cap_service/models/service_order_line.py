8ac02aaf622623f5721327c47cd1c2681fc02d5d
Bassim Elsamaloty <99155821+Captivea-BEL@users.noreply.github.com>
2024-11-14 16:57:14 -0500

    [ADD] - 139448 - Attachments and Chatter (#134)
    Task:
    
    https://www.captivea.com/web#id=139448&menu_id=462&cids=1%2C14&action=665&active_id=1926&model=project.task&view_type=form
    
    Deployment Notes:
    upgrade cap_service
    
    Release Notes:
    
    - Added Kanban as an image widget viewer. It will attempt to render the image if the attachment is an image otherwise it will display an icon for the mimetype. The create date is displayed and the menu will let you download it as well as a button to do the same thing at the bottom of the kanban
    - There is now a button that can be displayed on the service order line if the related task has attachments. This opens the aforementioned widget
    - Added chatter functionality to the service ordeer.
    - Implemented custom tracking for when Service Lines are added/deleted. Added will show the type/name as a dot list. The delete will show the sequence and the name as a dot list.
    - Implemented custom tracking for when Sales Orders are created/updated. Similarly Task create is also tracked in this same manner
    -  Added standard tracking to partner fields, state, fleet, and date fields

--- cap_service/models/service_order_line.py
+++ cap_service/models/service_order_line.py
@@ -1,5 +1,6 @@
-from odoo import  models, fields, api
+from odoo import  models, fields, api, _
 from odoo.exceptions import UserError
+from markupsafe import Markup
 
 
 class ServiceOrder(models.Model):
@@ -28,6 +29,8 @@ class ServiceOrder(models.Model):
     service_order_line_product_ids = fields.One2many('service.order.line.product', 'service_order_line_id',)
     service_order_line_service_ids = fields.One2many('service.order.line.service', 'service_order_line_id',)
     sequence = fields.Integer()
+    task_attachment_ids = fields.One2many('ir.attachment', related='task_id.attachment_ids')
+    task_attachment_count = fields.Integer(string="Attachment Count", compute="_compute_task_attachment_count", store=False)
     
     
     @api.depends('task_id.timesheet_ids.unit_amount')
@@ -54,6 +57,28 @@ class ServiceOrder(models.Model):
             record.cause = record.task_id.cause
             record.correction = record.task_id.correction
     
+    @api.depends('task_id')
+    def _compute_task_attachment_count(self):
+        for record in self:
+            record.task_attachment_count = self.env['ir.attachment'].search_count([
+                ('res_model', '=', 'project.task'),
+                ('res_id', '=', record.task_id.id)
+            ])
+    
+    def button_view_attachments(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Attachments',
+            'view_mode': 'kanban',
+            'res_model': 'ir.attachment',
+            'views': [
+                (self.env.ref('cap_service.view_attachment_kanban_preview').id, 'kanban'),
+            ],
+            'domain': [('res_model', '=', 'project.task'), ('res_id', '=', self.task_id.id)],
+            'target': 'current',
+        }
+
     def button_view_products(self):
         '''
             Service Line ID is passed through context
@@ -91,3 +116,32 @@ class ServiceOrder(models.Model):
             },
             'target': 'new',
         }
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        for service_order in records.mapped('service_order_id'):
+            service_order_lines = service_order.service_order_lines.filtered(lambda line: line.id in records.ids)
+            msg = Markup("<b>%s</b><ul>") % _("Service Lines Created:")
+            for line in service_order_lines:
+                msg += Markup("<li><b>Type:</b> %s, <b>Name:</b> %s</li>") % (
+                    line.ttype or 'N/A',
+                    line.name or 'N/A'
+                )
+            msg += Markup("</ul>")
+            service_order.message_post(body=msg,subtype_xmlid="mail.mt_note")
+        return records
+
+    def unlink(self):
+        service_orders = self.mapped('service_order_id')
+        for service_order in service_orders:
+            service_order_lines = self.filtered(lambda line: line.service_order_id == service_order)
+            msg = Markup("<b>%s</b><ul>") % _("Service Lines Deleted:")
+            for line in service_order_lines:
+                msg += Markup("<li><b>Sequence:</b> %s, <b>Description:</b> %s</li>") % (
+                    line.sequence or 'N/A',
+                    line.name or 'N/A'
+                )
+            msg += Markup("</ul>")
+            service_order.message_post(body=msg,subtype_xmlid="mail.mt_note")
+        return super().unlink()
