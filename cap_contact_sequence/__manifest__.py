4186d0e427a577584d8c15ecc7f4a713d9466cb5
captivea-asef <168564747+captivea-asef@users.noreply.github.com>
2024-11-25 14:34:40 -0500

    [add] 149462 implement sequence historical/new id fields for contact

---
+++ cap_contact_sequence/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Cap Contact Global Sequence Numbering System",
+
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+
+    'description': """
+Long description of module's purpose
+    """,
+
+    'author': "Captivea CANADA",
+    'website': "https://www.captivea.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['contacts'],
+
+    # always loaded
+    'data': [
+        'data/res_partner_data.xml',
+        'views/res_partner.xml',
+    ],
+
+}
+
