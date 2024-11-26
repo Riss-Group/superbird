db858ab738e56bf7eb1d01b94746e50f5179cd22
captivea-asef <168564747+captivea-asef@users.noreply.github.com>
2024-11-26 10:38:55 -0500

    [upd] 153863 Cores type of parts Feedbacks

--- cap_product_core_type/models/product.py
+++ cap_product_core_type/models/product.py
@@ -7,10 +7,32 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     has_core = fields.Boolean("Has Core Part")
+    is_core_type = fields.Boolean("is Core")
+
+    @api.onchange("is_core_type")
+    def _onchange_is_core_type(self):
+        if self.is_core_type :
+            core_part_categ = self.env.ref("cap_product_core_type.core_part_categ")
+            self.write({
+                'sale_ok': False,
+                'purchase_ok': False,
+                'categ_id': core_part_categ.id,
+                        })
 
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     has_core = fields.Boolean(related="product_tmpl_id.has_core", string="Has Core Part", readonly=False)
+    is_core_type = fields.Boolean(related="product_tmpl_id.is_core_type", string="Is Core", readonly=False)
     core_part_id = fields.Many2one('product.product', string="Core Part")
+
+    @api.onchange("is_core_type")
+    def _onchange_is_core_type(self):
+        if self.is_core_type :
+            core_part_categ = self.env.ref("cap_product_core_type.core_part_categ")
+            self.write({
+                'sale_ok': False,
+                'purchase_ok': False,
+                'categ_id': core_part_categ.id,
+                        })
