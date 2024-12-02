5851d4299bb7b7f8ab1ba2020ef9e1dcc8e8bc81
captivea-asef <168564747+captivea-asef@users.noreply.github.com>
2024-12-02 15:17:41 -0500

    [add] picking priority

---
+++ cap_picking_priority/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "cap picking priority",
+
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+
+    'description': """
+Long description of module's purpose
+    """,
+
+    'author': "Captivea",
+    'website': "https://www.captivea.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['stock'],
+    'data': [
+    ],
+
+}
+
