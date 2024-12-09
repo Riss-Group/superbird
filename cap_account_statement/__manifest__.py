0bb96a049f39f42889b0faf447b55f9bb802007a
Captivea-BEL <99155821+Captivea-BEL@users.noreply.github.com>
2024-12-09 10:26:44 -0500

    [ADD] - 139562 - Feedback - Customer Statement
    Feedback IDs:
    
    https://www.captivea.com/web#id=1363&cids=1%2C14&model=project.feedback&view_type=form
    
    https://www.captivea.com/web#id=1364&cids=1%2C14&active_id=1364&model=project.feedback&view_type=form
    
    Task:
    
    https://www.captivea.com/web#id=139562&cids=1%2C14&model=project.task&view_type=form
    
    Deployment Notes:
    
    Upgrade cap_account_statement
    
    Release Notes:
    
    - Added new field to company model to for 'statement display name'
    - If set, this will replace the company name on the statement subtotal sections
    - Added an 'if' condition on the report so that if the pst number is set it will be displayed (similar to currently existing vat functionality)
    - Prior to testing the companies/branches should have their values set on the company records. This would be locked to settings access rights

--- cap_account_statement/__manifest__.py
+++ cap_account_statement/__manifest__.py
@@ -3,7 +3,7 @@
     'name': 'CAP Account Statements',
     'category': 'Accounting',
     'summary': "CAP Account Statements",
-    'version': '1.0',
+    'version': '1.1',
     'author': 'Captivea software Consulting, Bassim Elsamaloty',
     'website': 'https://www.captivea.com/',
     'license': 'OPL-1',
@@ -13,10 +13,12 @@
     'depends': [
         'account', 
         'account_followup',
+        'l10n_ca'
         ],
     'data': [
         'data/data.xml',
         'report/customer_statement.xml',
+        'views/res_company.xml',
         'views/res_partner.xml',
     ],
     'installable': True,
