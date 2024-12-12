a56c4555953921d7eb0af1bdcd0ab94fd4b7c267
captivea-kha <khalid.hazam@captivea.com>
2024-12-12 08:47:11 -0500

    [WIP] AI integration

---
+++ dx_invoice_digitization/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'AI Purchase Order Digitalization (OCR)',
+    'version': '17.0.0.0',
+    'category': 'Extra Tools',
+    'summary': """Digitize Invoices,
+    """,
+    'description': """
+        This module allows users to digitize invoices, extract relevant information using OCR, and complete Odoo records
+         with the extracted data also using artificial intelligence.
+    """,
+    'author': "David Montero Crespo",
+    'website': 'https://odoonext.com',
+    'depends': ['account'],
+    'external_dependencies': {
+        'python': ['pytesseract', 'pypdf', 'pdf2image', 'numpy', 'Pillow', 'fuzzywuzzy'],
+    },
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/invoice_digitalize.xml',
+        'views/account_move.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'price': 125,
+    'auto_install': False,
+    'license': 'AGPL-3',
+    "images": ["static/ai_completion.gif"],
+}
