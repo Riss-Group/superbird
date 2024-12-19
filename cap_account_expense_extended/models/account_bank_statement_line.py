4cf134e90cc138f505cd80fc57afa0939f703561
captivea-darshan <darshan.kothiya@riss.group>
2024-12-19 10:26:04 -0500

    [IMP]change that made employee field company dependent on journal

--- cap_account_expense_extended/models/account_bank_statement_line.py
+++ cap_account_expense_extended/models/account_bank_statement_line.py
@@ -11,7 +11,7 @@ class AccountBankStatementLine(models.Model):
     def create_expense_action(self):
         if not self.journal_id.employee_id:
             raise ValidationError(_('Please define an employee on the journal'))
-        expense_dict = {'name': self.payment_ref, 'employee_id': self.journal_id.employee_id.id, 'date': self.date,
+        expense_dict = {'name': self.payment_ref, 'employee_id': self.journal_id.with_company(self.company_id).employee_id.id, 'date': self.date,
                         'total_amount_currency': abs(self.amount), 'currency_id': self.currency_id.id,
                         'company_id': self.company_id.id, 'product_id': self.company_id.expense_product_id.id,
                         'payment_mode': 'company_account'}
