2ce0423a3074105cfcb08f642fc211944013bcfb
captivea-asef <168564747+captivea-asef@users.noreply.github.com>
2025-01-18 18:04:48 -0500

    [add] purchase replacement

---
+++ cap_purchase_replacement/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "cap_purchase_replacement",
+    'license': 'OPL-1',
+
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+
+    'description': """
+Long description of module's purpose
+    """,
+
+    'author': "Captivea Canada",
+    'website': "https://www.captivea.com",
+    'version': '0.1',
+    'depends': ['cap_sale_replacement','purchase'],
+
+    # always loaded
+    'data': [
+    ],
+}
+
