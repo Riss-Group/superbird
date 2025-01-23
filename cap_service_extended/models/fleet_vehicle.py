7c8a656925489f69dcafe55501986105312bf752
captivea-kha <khalid.hazam@captivea.com>
2025-01-23 12:31:30 -0500

    [IMP] cap-service_extended make it import-friendly

--- cap_service_extended/models/fleet_vehicle.py
+++ cap_service_extended/models/fleet_vehicle.py
@@ -8,30 +8,41 @@ class FleetVehicle(models.Model):
     # purchase fields
     chassis_built_date = fields.Date('Chassis Built Date')
     chassis_manufacturer_arrival_date = fields.Date('Chassis Manufacturer Arrival Date')
-    bus_order_date = fields.Date('Bus Order Date', compute='_compute_dates')
+    bus_order_date = fields.Date('Bus Order Date', compute='_compute_dates', store=True, readonly=False)
     oem_production_date = fields.Date('OEM Production Date')
-    revised_pick_up_date = fields.Date('Revised Pick-up Date', compute='_compute_dates')
-    oem_ready_for_delivery_date = fields.Date('OEM Ready for Delivery Date') #TODO: confirm if it is to compute or it should be on stock move and not here?
-    dealer_arrival_date = fields.Date('Dealer Arrival', compute='_compute_dates')
-    pick_up_date = fields.Date('Pick-up Date', compute='_compute_dates')
+    revised_pick_up_date = fields.Date('Revised Pick-up Date', compute='_compute_dates', store=True, readonly=False)
+    oem_ready_for_delivery_date = fields.Date('OEM Ready for Delivery Date')  # TODO: confirm if it is to compute or move
+    dealer_arrival_date = fields.Date('Dealer Arrival', compute='_compute_dates', store=True, readonly=False)
+    pick_up_date = fields.Date('Pick-up Date', compute='_compute_dates', store=True, readonly=False)
 
     # sales field
-    sold_customer_date = fields.Date('Sold Customer Date', compute='_compute_dates')
-    sales_requested_delivery_date = fields.Date('Sales Requested Delivery Date', compute='_compute_dates')
-    ready_for_delivery_date = fields.Date('Ready for Delivery Date', compute='_compute_dates')
-    customer_delivered_date = fields.Date('Customer Delivered Date', compute='_compute_dates')
+    sold_customer_date = fields.Date('Sold Customer Date', compute='_compute_dates', store=True, readonly=False)
+    sales_requested_delivery_date = fields.Date('Sales Requested Delivery Date', compute='_compute_dates', store=True, readonly=False)
+    ready_for_delivery_date = fields.Date('Ready for Delivery Date', compute='_compute_dates', store=True, readonly=False)
+    customer_delivered_date = fields.Date('Customer Delivered Date', compute='_compute_dates', store=True, readonly=False)
 
     # accounting fields
-    oem_payment_due_date = fields.Date('OEM Payment Due Date', compute='_compute_dates')
+    oem_payment_due_date = fields.Date('OEM Payment Due Date', compute='_compute_dates', store=True, readonly=False)
     oem_body_payment_date = fields.Date('OEM Body Payment Date')
     oem_chassis_payment_date = fields.Date('OEM Chassis Payment Date')
 
     # service fields
-    ready_for_inspection_date = fields.Date('Ready for Inspection Date', compute='_compute_service_date')
+    ready_for_inspection_date = fields.Date('Ready for Inspection Date', compute='_compute_service_date', store=True, readonly=False)
     in_service_date = fields.Date('In Service Date')
 
 
-    @api.depends('fleet_move_line_ids')
+    @api.depends(
+        'fleet_move_line_ids',
+        'fleet_move_line_ids.picking_type_id.code',
+        'fleet_move_line_ids.move_id.purchase_line_id.order_id',
+        'fleet_move_line_ids.move_id.purchase_line_id.order_id.date_approve',
+        'fleet_move_line_ids.move_id.purchase_line_id.order_id.date_planned',
+        'fleet_move_line_ids.move_id.date',
+        'fleet_move_line_ids.move_id.purchase_line_id.order_id.invoice_ids.invoice_date_due',
+        'fleet_move_line_ids.picking_id.date_done',
+        'fleet_move_line_ids.move_id.sale_line_id.order_id.date_order',
+        'fleet_move_line_ids.move_id.sale_line_id.order_id.commitment_date'
+    )
     def _compute_dates(self):
         for rec in self:
             bus_order_date = False
@@ -57,17 +68,17 @@ class FleetVehicle(models.Model):
                     sales_requested_delivery_date = sale_order_id and sale_order_id[0].commitment_date or False
                     ready_for_delivery_date = move_line.move_id.date or False
                     customer_delivered_date = move_line.picking_id.date_done or False
-        rec.bus_order_date = bus_order_date
-        rec.pick_up_date = pick_up_date
-        rec.revised_pick_up_date = revised_pick_up_date
-        rec.oem_payment_due_date = oem_payment_due_date
-        rec.dealer_arrival_date = dealer_arrival_date
-        rec.sold_customer_date = sold_customer_date
-        rec.sales_requested_delivery_date = sales_requested_delivery_date
-        rec.ready_for_delivery_date = ready_for_delivery_date
-        rec.customer_delivered_date = customer_delivered_date
+            rec.bus_order_date = bus_order_date
+            rec.pick_up_date = pick_up_date
+            rec.revised_pick_up_date = revised_pick_up_date
+            rec.oem_payment_due_date = oem_payment_due_date
+            rec.dealer_arrival_date = dealer_arrival_date
+            rec.sold_customer_date = sold_customer_date
+            rec.sales_requested_delivery_date = sales_requested_delivery_date
+            rec.ready_for_delivery_date = ready_for_delivery_date
+            rec.customer_delivered_date = customer_delivered_date
 
-    @api.depends('service_order_ids')
+    @api.depends('service_order_ids.start_date')
     def _compute_service_date(self):
         for rec in self:
             rec.ready_for_inspection_date = rec.service_order_ids and rec.service_order_ids[0].start_date or False
