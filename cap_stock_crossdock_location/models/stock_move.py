f0bd05775cc2046e861531be22149d6b91c71d89
captivea-asef <168564747+captivea-asef@users.noreply.github.com>
2025-01-29 11:43:38 -0500

    [upd] Cross Dock process multiple products

--- cap_stock_crossdock_location/models/stock_move.py
+++ cap_stock_crossdock_location/models/stock_move.py
@@ -8,28 +8,29 @@ class StockMove(models.Model):
 
 
     def _action_assign(self, force_qty=False):
-        if self.picking_code == 'internal' and self.picking_type_id.is_pick:
-            incoming_waiting_picking = self.search([('product_id','=', self.product_id.id),('state','=','confirmed')])
-            incoming_waiting_picking._check_waiting_sale_picking()
+        for move in self :
+            if move.picking_code == 'internal' and move.picking_type_id.is_pick:
+                incoming_waiting_picking = self.search([('product_id','=', move.product_id.id),('state','=','confirmed')])
+                incoming_waiting_picking._check_waiting_sale_picking()
         res = super(StockMove, self)._action_assign(force_qty)
-        if self.picking_code == 'internal' and self.picking_type_id.is_put_away:
-            self.filtered(lambda m: m.picking_code == 'internal')._check_waiting_sale_picking()
+        for move in self:
+            if move.picking_code == 'internal' and move.picking_type_id.is_put_away:
+                self.filtered(lambda m: m.picking_code == 'internal')._check_waiting_sale_picking()
         return res
 
     def _check_waiting_sale_picking(self):
         for move in self:
-            outgoing_waiting_moves = self.search([('state','in', ['waiting']), ('picking_code','=', 'outgoing'),
+            outgoing_waiting_moves = self.search([('state','in', ['confirmed','partially_available']), ('picking_code','=', 'internal'), ('picking_type_id.is_pick','=', True),
                                                   ('product_id','=', move.product_id.id),('id','!=', move.id)])
-            if outgoing_waiting_moves:
-                outgoing_waiting_moves.sale_line_id.move_ids
+
             crossdock_location_id = self.warehouse_id.crossdock_location_id
             if outgoing_waiting_moves and crossdock_location_id:
                 sale_qty = sum(outgoing_waiting_moves.mapped('product_uom_qty'))
                 move.update_move_destination_location(crossdock_location=crossdock_location_id, sale_qty=sale_qty)
-                self.env.cr.commit
+        self.env.cr.commit
 
     def update_move_destination_location(self, crossdock_location=False, sale_qty=0):
-        if crossdock_location and sale_qty and crossdock_location not in self.picking_id.move_ids.mapped('location_dest_id'):
+        if crossdock_location and sale_qty and crossdock_location not in self.picking_id.move_ids.filtered(lambda m: m.product_id == self.product_id).mapped('location_dest_id'):
             if self.quantity > sale_qty:
                 #split line
                 self.copy({'location_dest_id': crossdock_location.id, 'product_uom_qty':sale_qty, 'quantity':sale_qty})
