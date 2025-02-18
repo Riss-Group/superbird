700d23d1f5c2e5a56f16b0025f40b18c2bc0b892
captivea-asef <168564747+captivea-asef@users.noreply.github.com>
2025-02-18 11:24:02 -0500

    [upd] limit core return to group core management, make cap_helpdesk inherit from cap_core

--- cap_helpdesk_stock/__manifest__.py
+++ cap_helpdesk_stock/__manifest__.py
@@ -15,13 +15,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['helpdesk_stock'],
+    'depends': ['cap_product_core_type'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/stock_picking_return.xml',
         'views/helpdesk_team.xml',
+        'views/helpdesk_ticket.xml',
         'views/stock_return_reason.xml',
         'reports/rma_return_slip.xml',
     ],
